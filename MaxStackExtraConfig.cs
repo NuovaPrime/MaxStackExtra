388b67bf0237754177440b60a593f48b6beb6efe
NuovaPrime <loganman51@hotmail.com>
2019-07-09 01:57:32 -0600

    Migration to 0.11 + modconfig
    I must wait for the next tmodloader patch to push due to an issue I've found with the range attribute and config values.

---
+++ MaxStackExtraConfig.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+using Terraria.ModLoader.Config.UI;
+using Terraria.UI;
+
+namespace MaxStackExtra
+{
+    public class MaxStackExtraConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [Label("Maximum stack for items")]
+        [Tooltip("Configure the maximum stack size for items.")]
+        [ReloadRequired]
+        [Range(0, int.MaxValue)]
+        [DefaultValue(9999)]
+        public int ItemStackValue { get; set; }
+
+        [Header("[c/ff0000:WARNING: May break prefixes and internal item data.]")]
+        [Label("Maximum stack for weapons")]
+        [Tooltip("Configure the maximum stack size for weapons.")]
+        [ReloadRequired]
+        [Range(0, int.MaxValue)]
+        [DefaultValue(1)]
+        public int WeaponStackValue { get; set; }
+
+        [Label("Maximum stack for equippables")]
+        [Tooltip("Configure the maximum stack size for armor and accessories.")]
+        [ReloadRequired]
+        [Range(0, int.MaxValue)]
+        [DefaultValue(1)]
+        public int EquipStackValue { get; set; }
+    }
+}
--- MaxStackExtraItem.cs
+++ MaxStackExtraItem.cs
@@ -1,32 +1,24 @@
 using Terraria;
 using Terraria.ModLoader;
 using System;
+using Terraria.ID;
 
 namespace MaxStackExtra
 {
     public class MaxStackExtraItem : GlobalItem
     {
-        
-        public override bool CloneNewInstances
-        {
-            get
-            {
-                return true;
-            }
-        }
+
+        public override bool CloneNewInstances => true;
+
         public override void SetDefaults(Item item)
         {
-            item.maxStack = 9999;
-            item.uniqueStack = false;
-        }
-        public override void Update(Item item, ref float gravity, ref float maxFallSpeed)
-        {
-
-            item.maxStack = 9999;
-            if(item.accessory)
-            {
-                item.maxStack = 9999;
-            }
+            if (item.damage > 0)
+                item.maxStack = mod.GetConfig<MaxStackExtraConfig>().WeaponStackValue;
+            else if (item.accessory || item.defense > 0)
+                item.maxStack = mod.GetConfig<MaxStackExtraConfig>().EquipStackValue;
+            else if (!(item.type == ItemID.CopperCoin || item.type == ItemID.SilverCoin || item.type == ItemID.GoldCoin || item.type == ItemID.PlatinumCoin))
+                item.maxStack = mod.GetConfig<MaxStackExtraConfig>().ItemStackValue;
+            base.SetDefaults(item);
         }
     }
 }
