8936afefc4d3ecf27a16965fe5a87df113fa55df
NuovaPrime <loganman51@hotmail.com>
2019-07-18 21:56:48 -0600

    Fixed coin stacking bug

--- MaxStackExtraItem.cs
+++ MaxStackExtraItem.cs
@@ -12,15 +12,19 @@ namespace MaxStackExtra
 
         public override void SetDefaults(Item item)
         {
-            if (item.damage > 0)
-                item.maxStack = mod.GetConfig<MaxStackExtraConfig>().WeaponStackValue;
-            else if (item.createTile > 0)
-                item.maxStack = mod.GetConfig<MaxStackExtraConfig>().TileStackValue;
-            else if (item.accessory || item.defense > 0)
-                item.maxStack = mod.GetConfig<MaxStackExtraConfig>().EquipStackValue;
-            else if (!(item.type == ItemID.CopperCoin || item.type == ItemID.SilverCoin || item.type == ItemID.GoldCoin || item.type == ItemID.PlatinumCoin))
-                item.maxStack = mod.GetConfig<MaxStackExtraConfig>().ItemStackValue;
-            base.SetDefaults(item);
+            if (!(item.type == ItemID.CopperCoin || item.type == ItemID.SilverCoin || item.type == ItemID.GoldCoin || item.type == ItemID.PlatinumCoin))
+            {
+                if (item.damage > 0)
+                    item.maxStack = mod.GetConfig<MaxStackExtraConfig>().WeaponStackValue;
+                else if (item.createTile > 0)
+                    item.maxStack = mod.GetConfig<MaxStackExtraConfig>().TileStackValue;
+                else if (item.accessory || item.defense > 0)
+                    item.maxStack = mod.GetConfig<MaxStackExtraConfig>().EquipStackValue;
+                else
+                    item.maxStack = mod.GetConfig<MaxStackExtraConfig>().ItemStackValue;
+            }
+            else
+                base.SetDefaults(item);
         }
     }
 }
